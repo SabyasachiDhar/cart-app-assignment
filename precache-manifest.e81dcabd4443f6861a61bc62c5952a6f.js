1e99b56ae58fd99d21cbe0405aa9b5f85307a6c3
Sabyasachi Dhar <sadhar11@in.ibm.com>
2019-12-17 14:58:11 +0530

    Updates

---
+++ precache-manifest.e81dcabd4443f6861a61bc62c5952a6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81c80138adb002ba90d0a6441c6acfc",
+    "url": "/cart-app-assignment/index.html"
+  },
+  {
+    "revision": "337a2985ccbd1b09d976",
+    "url": "/cart-app-assignment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "653ee15f4f36e7e0e843",
+    "url": "/cart-app-assignment/static/js/2.27978dca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cart-app-assignment/static/js/2.27978dca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "337a2985ccbd1b09d976",
+    "url": "/cart-app-assignment/static/js/main.528c51ad.chunk.js"
+  },
+  {
+    "revision": "842ae7ee0c23c6a36ada",
+    "url": "/cart-app-assignment/static/js/runtime-main.e2010aba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cart-app-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
